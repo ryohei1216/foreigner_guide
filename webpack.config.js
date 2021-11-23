7e6829b7cd76e3c84d0c973dab401b48b5026d8a
miyamoto <ryohei1000000000@gmail.com>
2021-11-24 00:56:19 +0900

    jest

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -6,7 +7,7 @@ module.exports = {
   output: {
     //  出力ファイルのディレクトリ名
     filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "build"),
   },
   module: {
     rules: [
@@ -19,6 +20,13 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Foreigner Guide",
+      template: "public/index.html",
+      filename: "index.html",
+    }),
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, "public"),
