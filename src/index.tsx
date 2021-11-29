df2da67e8895dd4b8f8ef224591917e2be50fdff
miyamoto <ryohei1000000000@gmail.com>
2021-11-29 18:59:22 +0900

    pre

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 
-import { store } from "./__test__/features/customCounter/store";
+import store from "./utils/store/index";
 
 ReactDOM.render(
   <React.StrictMode>
