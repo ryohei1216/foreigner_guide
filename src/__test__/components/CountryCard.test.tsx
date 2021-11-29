6fafbdd1812451db586eeb86c68a6b965238124d
miyamoto <ryohei1000000000@gmail.com>
2021-11-30 02:04:01 +0900

    CountryCardのテスト

---
+++ src/__test__/components/CountryCard.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen, cleanup } from "@testing-library/react";
+import { CountryCard } from "../../components/CountryCard";
+
+afterEach(() => cleanup());
+
+describe("components/CountryCard", () => {
+  test("CommonCardにpropsを渡してタイトル表示", () => {
+    render(
+      <CountryCard webUrl="weburl" imageUrl="imageUrl" title="アメリカ" />
+    );
+    console.log(screen.debug());
+    expect(screen.getByTestId("title")).toHaveTextContent("アメリカ");
+  });
+});
